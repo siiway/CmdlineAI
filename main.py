a17ba21cdb0c7e4458fc671470552275698ac4ed
wyf9 <wyf9@wyf9.top>
2024-07-20 00:11:20 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,58 @@
+#!/usr/bin/python3
 # coding: utf-8
 
-import utils as u
+from utils import utils as utils_init
 from libs.getchar import getChar as getchar
-import config.config as config_init
+from config import config as config_init
 config = config_init()
+u = utils_init()
 
+
+def Main():
+    u.info('''---
+    Welcome to CmdlineAI v1!
+    Copyright (c)2024 wyf9. All rights reserved.
+    ''')
+
+    u.env_debug = config.cfg['debug']
+    u.debug('Debug ON')
+
+    while True:
+        u.info('''---
+        [Tip]
+        n -> New chat
+        c -> Chat list
+        q -> Quit
+        s -> Settings
+        ''')
+
+        while True:
+            gchr = getchar()
+            u.debug(f'getChar: {repr(gchr)}')
+            match gchr:
+                case 'q' | 'Q':
+                    u.info('Quit.')
+                    exit(0)
+                case 's' | 'S':
+                    u.info('Settings')
+                    Settings()
+                    break
+                case 'n' | 'N':
+                    u.info('NewChat')
+                case 'c' | 'C':
+                    u.info('ChatList')
+                case '\x03' | '\x1a':  # ^C / ^Z
+                    u.warning('Received ^C/^Z, quitting.')
+                    exit(1)
+                case _:
+                    pass
+
+def Settings():
+    pass
+
+if __name__ == "__main__":
+    try:
+        Main()
+    except err:
+        u.error('An unexcept error. Exiting.')
+        
--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
 from colorama import Fore, Style
-_debug = False
-def info(msg):
-    print(f'{Fore.GREEN}[I]{Style.RESET_ALL} {msg}')
-def warning(msg):
-    print(f'{Fore.YELLOW}[W]{Style.RESET_ALL} {msg}')
-def error(msg):
-    print(f'{Fore.RED}[E]{Style.RESET_ALL} {msg}')
-def debug(msg):
-    if _debug:
+class utils:
+    env_debug = False
+    def info(self, msg):
+        print(f'{Fore.GREEN}[I]{Style.RESET_ALL} {msg}')
+    def warning(self, msg):
+        print(f'{Fore.YELLOW}[W]{Style.RESET_ALL} {msg}')
+    def error(self, msg):
         print(f'{Fore.RED}[E]{Style.RESET_ALL} {msg}')
+    def debug(self, msg):
+        if self.env_debug:
+            print(f'{Fore.BLUE}[D]{Style.RESET_ALL} {msg}')
